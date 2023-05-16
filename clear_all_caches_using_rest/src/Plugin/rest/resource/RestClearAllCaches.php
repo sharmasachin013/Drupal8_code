cb3aa580ab2b037681fc55fdc1ebd3114c68f3ee
Sachin Sharma <sharma.sachin013@gmail.com>
2023-05-16 15:55:35 +0530

    custom RESTful Web Service in Drupal 8
    custom RESTful Web Service in Drupal 8

---
+++ clear_all_caches_using_rest/src/Plugin/rest/resource/RestClearAllCaches.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\clear_all_caches_using_rest\Plugin\rest\resource;
+
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Provides a resource to get view modes by entity and bundle.
+ *
+ * @RestResource(
+ *   id = "rest_clear_all_caches",
+ *   label = @Translation("Rest clear all caches"),
+ *   uri_paths = {
+ *     "create" = "/rest/clear/all/caches"
+ *   }
+ * )
+ */
+class RestClearAllCaches extends ResourceBase {
+
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->logger = $container->get('logger.factory')->get('clear_all_caches_using_rest');
+    $instance->currentUser = $container->get('current_user');
+    return $instance;
+  }
+
+    /**
+     * Responds to POST requests.
+     *
+     * @param string $payload
+     *
+     * @return \Drupal\rest\ModifiedResourceResponse
+     *   The HTTP response object.
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     *   Throws exception expected.
+     */
+    public function post($payload) {
+
+        // You must to implement the logic of your REST Resource here.
+        // Use current user after pass authentication to validate access.
+        if (!$this->currentUser->hasPermission('access content')) {
+            throw new AccessDeniedHttpException();
+        }
+
+        drupal_flush_all_caches();
+        $payload['message'] = 'Caches cleared.';
+        return new ModifiedResourceResponse($payload, 200);
+    }
+
+}
